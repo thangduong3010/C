de61ba897b7345167e0ace81d2364e7cc6c7e5df
thangduong3010 <thangduong.hanu@gmail.com>
2015-11-21 16:24:40 +0700

    dot

--- c_tutorial/dummy.c
+++ c_tutorial/dummy.c
@@ -1,23 +1,45 @@
-#include <stdio.h>
+/*
+* Program to play around
+* for studying purpose only
+*/
+#include <stdio.h>	// hearder file
 
 
-int main(void)
+int main()
 {
-    int value[10];
-    int index;
+	int count = 0;
+	char card_name[3];
+	while (card_name[0] != 'X'){
+		puts("Enter the card name: ");
+		scanf("%2s", card_name);
+		int val = 0;
+		switch(card_name[0]){
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				break;
+			case 'A':
+				val = 11;
+				break;
+			case 'X':
+				puts("Err");
+				continue;
+			default:
+				val = atoi(card_name);
+				if (val < 1 || val > 10){
+					puts("Error!");
+					continue;
+				}
+		}
+		/* Check if the value is 3 to 6 */
+		if (val > 2 && val < 7)
+			count++;
+		/* Otherwise check if the card was 10, J, Q, K */
+		else if (val == 10)
+			count--;
+	printf("Current count: %i\n", count);
+	}
+	return 0;
 
-    /*value[0] = 197;
-    value[1] = -100;
-    value[5] = 350;
-    value[3] = value[0] + value[5];
-    value[9] = value[5] / 10;
-    --value[2];*/
-
-    for(index = 0; index < 10; ++index)
-        value[index] = 0;
-
-    for(index = 0; index < 10; ++index)
-        printf("value[%i] = %i\n", index, value[index]);
-
-    return 0;
 }
